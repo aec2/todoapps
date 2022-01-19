4345a3df5a93fcfba75814d302d54c2a59236852
Abdullah Enes Can <aenesc1@gmail.com>
2022-01-20 00:51:27 +0300

    End of section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useToDoLayerValue } from "./context/ToDoContext";
+import ToDoList from "./components/ToDoList";
 
-function App() {
+const App = () => {
+  const [{ todos }, dispatch] = useToDoLayerValue();
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!content) return null;
+
+    const newToDo = {
+      id: Math.floor(Math.random() * 4288978435),
+      content,
+      isCompleted: false,
+    };
+
+    dispatch({
+      type: "ADD_TODO",
+      payload: newToDo,
+    });
+
+    setContent("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="todo-form">
+        <input
+          type="text"
+          className="todo-input"
+          onChange={(event) => setContent(event.target.value)}
+          value={content}
+        ></input>
+        <button className="todo-button">Ekle</button>
+      </form>
+      {/* TODO: Todo listesi */}
+      {/* <ToDoList todos={todos} /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/context/reducer.js
@@ -0,0 +1,21 @@
+export const initialState = {
+  todos: [
+    {
+      id: 0,
+      content: "Ben ilk todo'yum.😊",
+    },
+  ],
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "ADD_TODO":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+export default reducer;
