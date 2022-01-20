2e0eb61c27d9d3cfe4ba9f2410f172a6352c2dd2
Abdullah Enes Can <aenesc1@gmail.com>
2022-01-21 00:39:28 +0300

    To Do App 2 begining

---
+++ to-do-app-2/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+//Importing Componets
+
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Enes's Todo List</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
